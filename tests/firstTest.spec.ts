b13f4c58502996280c6be2a7fd252b0d850c1a97
Atif <atestsha@gmail.com>
2025-02-19 16:32:13 +0000

    Add child element locator notes

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,21 +1,19 @@
 import {test} from '@playwright/test'
 
-//test.beforeEach(async({page}) => {
- //   await page.goto('http://localhost:4200/')
- //   await page.getByText('Forms').click()
- //   await page.getByText('Form Layouts').click()
-//})
+test.beforeEach(async({page}) => {
+   await page.goto('http://localhost:4200/')
+   await page.getByText('Forms').click()
+   await page.getByText('Form Layouts').click()
+})
 
 test('Locator syntax rules', async({page}) => {
-    await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
-    await page.getByText('Form Layouts').click()
-    await page.locator('input').first().click()
+    //await page.locator('input').first().click()
 
     //By tag
     page.locator('input')
     // by ID
-    page.locator('#inputEmail1')
+ //
+ //    await page.locator('#inputEmail1').click()
     // by class value
     page.locator('.shape-rectangle')
     //by attribute
@@ -33,3 +31,26 @@ test('Locator syntax rules', async({page}) => {
 
 })
 
+test('User facing Locators', async({page}) => {
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTestId('SignIn').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+})
+
+test.only('locating child elements', async({page}) => {
+    //compact finding child element 
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click
+    //alternative find child element
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 1")').click
+    //mix different types
+    await page.locator('nb-card').getByRole('button').click()
+})
